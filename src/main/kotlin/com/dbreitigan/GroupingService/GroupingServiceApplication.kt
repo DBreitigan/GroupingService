21e9360141830eb2f186f71322598d9c51a1574d
Daniel Breitigan <danthemanb3@gmail.com>
2018-07-23 13:07:38 -0600

    Initialize project

---
+++ src/main/kotlin/com/dbreitigan/GroupingService/GroupingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dbreitigan.GroupingService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupingServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroupingServiceApplication>(*args)
+}
